d4bef4698bc75a2019c6409930b5d21165ee56e5
Orkhon Purev <orkhon@Orkhons-MacBook-Air.local>
2024-03-21 17:29:07 +0800

    register

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,137 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <div
+      className="h-screen w-full flex flex-col items-center"
+      style={{
+        backgroundImage:
+          "url('https://www.pizzahut.mn/assets/Images/background.jpg')",
+      }}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="rounded-full 2xl:w-[72px] xl:w-[72px] lg:w-[72px] md:w-[72px] sm:w-[72px] 2xl:h-[72px] xl:h-[72px] lg:h-[72px] md:h-[72px] sm:h-[72px] w-[52px] h-[52px] bg-white absolute 2xl:mt-4 xl:mt-4 lg:mt-4 md:mt-4 sm:mt-4 drop-shadow-md flex items-center justify-center">
+        <img
+          src="https://www.pizzahut.mn/assets/Images/pizzahut_logo.png"
+          className="2xl:w-16 xl:w-16 lg:w-16 md:w-16 sm:w-16 w-[56px]"
+        ></img>
+      </div>
+      <div className="2xl:h-16 xl:h-16 lg:h-12 md:h-12 sm:h-12 h-8  w-full bg-white flex"></div>
+      <div className="flex flex-row my-8 flex items-center 2xl:w-full xl:w-full lg:w-full md:w-full sm:w-3/4 w-11/12">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2.8"
+          stroke="currentColor"
+          className="w-6 h-6 2xl:ml-32 xl:ml-28 md:ml-24 md:ml-20 ml-0 stroke-black"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+        <p className="text-lg text-slate-900 font-bold flex ml-1">Буцах</p>
+      </div>
+
+      <div className="2xl:w-1/2 xl:w-1/2 lg:w-2/3 md:w-3/4 sm:w-3/4 w-11/12 bg-white drop-shadow-lg rounded-md flex justify-center items-center flex-col">
+        <div className="2xl:w-2/3 xl:w-2/3 lg:w-2/3 md:w-2/3 sm:w-4/5 w-11/12 flex items-center justify-center">
+          <p className="mt-8 text-xl text-black 2xl:text-md xl:text-md lg:text-md md:text-md sm:text-md flex font-bold justify-center items-center">
+            Байхгүй бол бүртгүүлэх боломжтой Пицца Хат
+          </p>
+        </div>
+        <div className="2xl:w-2/3 xl:w-2/3 lg:w-2/3 md:w-2/3 sm:w-4/5 w-11/12 flex flex-col">
+          <div className=" w-full  mt-6 flex flex-col">
+            <div className="flex flex-row">
+              <p className="text-sm 2xl:text-sm xl:text-sm lg:text-sm md:text-sm sm:text-sm text-xs text-slate-900 ">
+                Овог, Нэр{" "}
+              </p>
+              <p className="text-red-600 ml-1">*</p>
+            </div>
+            <input
+              className="mt-1 w-full placeholder-slate-500 2xl:text-sm xl:text-sm lg:text-sm sm:text-sm text-xs h-12 rounded-md border-[1px] p-2 text-sm border-slate-300 focus:border-blue-400 flex"
+              placeholder="Овог, Нэрээ бичнэ үү "
+            ></input>
+          </div>
+          <div className=" w-full mt-1">
+            <div className="flex flex-row">
+              <p className="text-sm 2xl:text-sm xl:text-sm lg:text-sm md:text-sm sm:text-sm text-xs text-slate-900">
+                Утасны дугаар
+              </p>
+              <p className="text-red-600 ml-1">*</p>
+            </div>
+            <div className="w-full flex flex-row ">
+              <div className="w-20 h-12 border-[1px] border-slate-300 bg-slate-100 mt-1 rounded-sm flex items-center">
+                <p className="text-sm 2xl:text-sm xl:text-sm lg:text-sm md:text-sm sm:text-sm text-xs text-slate-400 pl-4">
+                  976
+                </p>
+              </div>
+              <input
+                className="mt-1 w-full placeholder-slate-500 h-12 2xl:text-sm xl:text-sm lg:text-sm sm:text-sm text-xs rounded-sm border-[1px] p-2 text-sm border-slate-300 focus:border-blue-400 flex"
+                placeholder=" Утасны дугаараа оруулна уу"
+              ></input>
+            </div>
+          </div>
+
+          <div className=" w-full ">
+            <div className="flex flex-row">
+              <p className="text-sm mt-4 2xl:text-sm xl:text-sm lg:text-sm md:text-sm sm:text-sm text-xs text-slate-900">
+                И-Мэйл хаяг
+              </p>
+            </div>
+
+            <input
+              className="mt-1 w-full placeholder-slate-500 h-12 2xl:text-sm xl:text-sm lg:text-sm sm:text-sm text-xs rounded-md border-[1px] p-2 text-sm border-slate-300 focus:border-blue-400 flex"
+              placeholder="И-Мэйл хаягаа оруулна уу"
+            ></input>
+          </div>
+
+          <div className=" w-full mt-1">
+            <div className="flex flex-row">
+              <p className="text-sm 2xl:text-sm xl:text-sm lg:text-sm md:text-sm sm:text-sm text-xs text-slate-900">
+                Нууц Үг
+              </p>
+              <p className="text-red-600 ml-1">*</p>
+            </div>
+
+            <input
+              className="mt-1 w-full placeholder-slate-500 h-12 2xl:text-sm xl:text-sm lg:text-sm sm:text-sm text-xs rounded-md border-[1px] p-2 text-sm border-slate-300 focus:border-blue-400 flex"
+              placeholder="Нууц үг"
+            ></input>
+            <div className="flex flex-row">
+              <input
+                type="checkbox"
+                className=" mt-3 checked:bg-blue-500 ..."
+              />
+              <p className="text-sm 2xl:text-sm xl:text-sm lg:text-sm md:text-sm sm:text-sm text-xs ml-2 mt-3 text-slate-900 flex flex-row">
+                Зөвшөөрөх Пицца Хат
+                <p className="text-green-700 underline underline-offset-2 ml-1">
+                  Үйлчилгээний нөхцөл
+                </p>
+              </p>
+            </div>
+            <button className="w-full h-[48px] bg-[#0a8020] mt-4 rounded-md text-white ">
+              Бүртгүүлэх
+            </button>
+          </div>
+          <div className="grid grid-rows-2">
+            <div className="w-full h-[1px] bg-slate-200 mt-5 "></div>
+            <div className="flex justify-center mt-2">
+              <p className="2xl:text-md xl:text-md lg:text-md md:text-md sm:text-md text-sm text-slate-900">
+                Хэрэв та бүртгэлтэй бол?
+              </p>
+              <p className="2xl:text-md xl:text-md lg:text-md md:text-md sm:text-md text-sm text-green-700 underline underline-offset-2 ml-2">
+                Нэвтрэх
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <img
+        className="w-16 fixed mt-[700px] mr-[1330px]"
+        src="https://erxes.tavanbogd.mn/gateway/read-file?key=0.12943882451121258SauceOnlyOnWhiteRGB.PNG"
+      ></img>
+    </div>
   );
 }
